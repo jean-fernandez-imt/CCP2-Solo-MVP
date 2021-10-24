9e25bd66eace0706c764a70a344e83ec95a9e6a8
JeanFernandez <jean.fernandez_imt@outlook.com>
2021-10-24 18:46:27 +0900

    Implement basic REST api

--- AngularServer/server.js
+++ AngularServer/server.js
@@ -1,14 +1,77 @@
 const express = require('express');
 const path = require('path');
 
+const { PrismaClient } =  require('@prisma/client');
+const prisma = new PrismaClient()
+
 const app = express();
+app.use(express.json())
 
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, "..", "dist", "CCP2-Solo-MVP")));
 
-// Always return the main index.html, since we are developing a single page application
-app.get("*", (req, res) => {
+// From here it will get redirected to "/manager"
+app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "..", "dist", "CCP2-Solo-MVP", "index.html"));
+});
+
+// API Basic Interface
+app.get("/api", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "api.html"));
+});
+
+/* Projects CRUD Operations */
+// Get all projects information
+app.get("/api/projects", async (req, res) => {
+  const result = await prisma.project.findMany({
+    include: {
+      Items: true,
+    }
+  });
+
+  res.json(result);
+});
+
+// Get project by Id
+app.get("/api/project/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const result = await prisma.project.findUnique({
+    where: {
+      Id: id,
+    },
+    include: {
+      Items: true,
+    }
+  });
+
+  res.json(result);
+});
+
+// Create a new project
+app.post(`/project`, async (req, res) => {
+  const { name } = req.body;
+
+  const result = await prisma.project.create({
+    data: {
+      Name: name,
+    }
+  });
+
+  res.json(result);
+});
+
+// Delete a project
+app.post(`/project/:id`, async (req, res) => {
+  const { id } = req.params;
+
+  const result = await prisma.project.delete({
+    where: {
+      Id: id,
+    }
+  });
+
+  res.json(result);
 });
 
 (() => {
